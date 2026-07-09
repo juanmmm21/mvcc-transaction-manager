d2aedb32dceb1fe3d85a9e735ac9e7aa2bcbd76c
juanmmm21 <martoscuevasjuan@gmail.com>
2026-07-09 11:57:19 +0200

    feat: public package API exports

--- src/mvcc_transaction_manager/__init__.py
+++ src/mvcc_transaction_manager/__init__.py
@@ -8,4 +8,40 @@ lecturas bloqueen nunca a las escrituras concurrentes.
 
 from __future__ import annotations
 
+from .models import (
+    CommitSeq,
+    IsolationLevel,
+    MvccError,
+    RowId,
+    RowVersion,
+    SerializationConflictError,
+    Snapshot,
+    Transaction,
+    TransactionId,
+    TransactionNotActiveError,
+    TransactionStatus,
+    WriteConflictError,
+)
+from .pipeline import MVCCTransactionManager
+from .protocols import RowStore
+from .store import InMemoryRowStore
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "CommitSeq",
+    "InMemoryRowStore",
+    "IsolationLevel",
+    "MVCCTransactionManager",
+    "MvccError",
+    "RowId",
+    "RowStore",
+    "RowVersion",
+    "SerializationConflictError",
+    "Snapshot",
+    "Transaction",
+    "TransactionId",
+    "TransactionNotActiveError",
+    "TransactionStatus",
+    "WriteConflictError",
+]
